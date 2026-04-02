06c24b6d1cd7c74e4a279491f3fc792c378c25cb
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-04-01 23:18:17 -0700

    feat: added login endpoint

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { authenticateUser } from '../middleware/authentication.js';
-import { registerValidation } from '../validations/authValidation.js';
+import {
+  registerValidation,
+  loginValidation,
+} from '../validations/authValidation.js';
 import validateRequest from '../middleware/validateRequest.js';
-import { register } from '../controllers/authController.js';
+import { register, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', registerValidation, validateRequest, register);
+router.post('/login', loginValidation, validateRequest, login);
 
 export default router;
