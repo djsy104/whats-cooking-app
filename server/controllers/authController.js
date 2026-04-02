99b99a5ea1b46656ffdbbc4f5ca466a6738ec67f
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-04-01 23:31:17 -0700

    feat: Added getUser logic and endpoint
    - Removed password from returned user object

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,16 +3,6 @@ import { StatusCodes } from 'http-status-codes';
 import { hashPassword, comparePasswords } from '../utils/password.js';
 import { createJWT } from '../utils/token.js';
 
-export const getUsers = async (req, res) => {
-  try {
-    const result = await query('SELECT * from users;');
-    console.log(result.rows);
-    return res.json(result.rows);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -45,7 +35,7 @@ export const login = async (req, res) => {
       `
       SELECT id, name, email, password_hash
       FROM users
-      where email = $1
+      WHERE email = $1
       LIMIT 1
       `,
       [email]
@@ -57,8 +47,40 @@ export const login = async (req, res) => {
     const isMatch = await comparePasswords(password, user.password_hash);
     if (!isMatch) throw new Error('Incorrect password!');
 
+    const safeUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
     const token = createJWT({ userId: user.id, name: user.name });
-    res.status(StatusCodes.OK).json({ user, token });
+    return res.status(StatusCodes.OK).json({ user: safeUser, token });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
+
+// Returns the current user
+export const getUser = async (req, res) => {
+  try {
+    const result = await query(
+      `
+      SELECT id, name, email
+      FROM users
+      WHERE id = $1
+      `,
+      [req.user.userId]
+    );
+
+    const user = result.rows[0];
+    if (!user) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'User not found' });
+    }
+
+    return res.status(StatusCodes.OK).json({ user });
   } catch (error) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -5,11 +5,12 @@ import {
   loginValidation,
 } from '../validations/authValidation.js';
 import validateRequest from '../middleware/validateRequest.js';
-import { register, login } from '../controllers/authController.js';
+import { register, login, getUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', registerValidation, validateRequest, register);
 router.post('/login', loginValidation, validateRequest, login);
+router.get('/me', authenticateUser, getUser);
 
 export default router;
