cec0d3c5a8e2cb4a6e57cf424ca684d26f4076a1
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-04-03 23:59:22 -0700

    feat: implemented create library

--- server/controllers/libraryController.js
+++ server/controllers/libraryController.js
@@ -2,7 +2,29 @@ import { query } from '../config/db.js';
 import { StatusCodes } from 'http-status-codes';
 
 export const createLibrary = async (req, res) => {
-  return res.send('Create library');
+  try {
+    const { name, description } = req.body;
+    const userId = req.user.userId;
+
+    if (!name) throw new Error('Name is required');
+
+    const result = await query(
+      `
+      INSERT INTO libraries (user_id, name, description)
+      VALUES ($1, $2, $3)
+      RETURNING id, user_id, name, description
+      `,
+      [userId, name, description]
+    );
+
+    const library = result.rows[0];
+
+    return res.status(StatusCodes.CREATED).json({ library });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
 export const getLibraries = async (req, res) => {
